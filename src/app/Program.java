3c72c649206c9256c7515a62c5bea8771efe6525
Vitor Vilela <vitorvilella123@gmail.com>
2023-10-20 01:53:43 -0300

    Class department

---
+++ src/app/Program.java
@@ -0,0 +1,19 @@
+package app;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		
+		
+		sc.close();
+	}
+
+}
