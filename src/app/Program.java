c4330dfbb9127a3718d3986e22d296fa200701e7
Vitor Vilela <vitorvilella123@gmail.com>
2023-10-20 13:08:23 -0300

    Insert

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
@@ -14,37 +15,40 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Locale.setDefault(Locale.US);
-		
-			
+
 		SellerDAO sellerDAO = DAOFactory.createSellerDao();
-		
+
 		System.out.print("Digite o ID do vendedor: ");
-		
+
 		int n = sc.nextInt();
-		
-		Seller seller = sellerDAO.findById(n);		
+
+		Seller seller = sellerDAO.findById(n);
 		System.out.println(seller);
-		
+
 		System.out.println("Vendedor por departamento");
-		
+
 		System.out.print("ID do Departamento: ");
 		int x = sc.nextInt();
-		
+
 		Department department = new Department(x, null);
-		List <Seller> list = sellerDAO.findByDepartment(department);
-		
+		List<Seller> list = sellerDAO.findByDepartment(department);
+
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("Todos os vendedores");
 		list = sellerDAO.findAll();
-		
+
 		for (Seller slr : list) {
 			System.out.println(slr);
 		}
-		
-		
+
+		System.out.println("inserindo vendedor");
+		Seller newSeller = new Seller(null, "Greg Mendez", "greg@gmail.com", new Date(), 3590.0, department);
+		sellerDAO.insert(newSeller);
+		System.out.println(newSeller);
+
 		sc.close();
 	}
 
--- src/model/dao/impl/SellerDAOjdbc.java
+++ src/model/dao/impl/SellerDAOjdbc.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,16 +17,46 @@ import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDAOjdbc implements SellerDAO {
-	
+
 	private Connection conn;
-	
-	public SellerDAOjdbc (Connection conn) {
+
+	public SellerDAOjdbc(Connection conn) {
 		this.conn = conn;
 	}
 
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement pst = null;
+
+		try {
+			pst = conn.prepareStatement("INSERT INTO seller " 
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES " + "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+
+			pst.setString(1, obj.getName());
+			pst.setString(2, obj.getEmail());
+			pst.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			pst.setDouble(4, obj.getBaseSalary());
+			pst.setInt(5, obj.getDepartment().getId());
+
+			int rowsAffected = pst.executeUpdate();
+
+			if (rowsAffected > 0) {
+				ResultSet rs = pst.getGeneratedKeys();
+
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				DB.closeResultSet(rs);
+			} else {
+				throw new DbException("Erro! Nenhuma linha foi afetada");
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(pst);
+		}
 
 	}
 
@@ -43,60 +74,59 @@ public class SellerDAOjdbc implements SellerDAO {
 
 	@Override
 	public Seller findById(Integer id) {
-		
+
 		PreparedStatement pst = null;
 		ResultSet rs = null;
-		
+
 		try {
 			pst = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "WHERE seller.Id = ? ");
-			
-			pst.setInt(1, id);			
-			rs = pst.executeQuery(); // por padrão, essa consulta retorna como 0. Sendo assim, criamos uma condição if com rs.next() para pegar os dados desejados
-			
+					"SELECT seller.*,department.Name as DepName " 
+						+ "FROM seller INNER JOIN department "
+						+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ? ");
+
+			pst.setInt(1, id);
+			rs = pst.executeQuery();
+
 			if (rs.next()) {
-				
-				Department dep = instantiateDepartment(rs);				
+
+				Department dep = instantiateDepartment(rs);
 				Seller seller = instantiateSeller(rs, dep);
-				
-				return seller;				
+
+				return seller;
 			}
-			
-			return null;		
-			
+
+			return null;
+
 		} catch (SQLException e) {
-			
+
 			throw new DbException(e.getMessage());
 		} finally {
-			
+
 			DB.closeStatement(pst);
 			DB.closeResultSet(rs);
 		}
-		
-		}
+
+	}
 
 	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
 		Seller seller = new Seller();
-		
+
 		seller.setId(rs.getInt("Id"));
 		seller.setName(rs.getString("Name"));
 		seller.setEmail(rs.getString("Email"));
 		seller.setBaseSalary(rs.getDouble("BaseSalary"));
 		seller.setBirthDate(rs.getDate("BirthDate"));
 		seller.setDepartment(dep);
-		
+
 		return seller;
 	}
 
 	private Department instantiateDepartment(ResultSet rs) throws SQLException {
 		Department dep = new Department();
-		
-		dep.setId(rs.getInt("DepartmentId"));				
+
+		dep.setId(rs.getInt("DepartmentId"));
 		dep.setName(rs.getString("DepName"));
-		
+
 		return dep;
 	}
 
@@ -104,39 +134,38 @@ public class SellerDAOjdbc implements SellerDAO {
 	public List<Seller> findAll() {
 		PreparedStatement pst = null;
 		ResultSet rs = null;
-		
+
 		try {
 			pst = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "ORDER BY Name ");
-			
-			rs = pst.executeQuery(); // por padrão, essa consulta retorna como 0. Sendo assim, criamos uma condição if com rs.next() para pegar os dados desejados
-			
+					"SELECT seller.*,department.Name as DepName " 
+						+ "FROM seller INNER JOIN department "
+						+ "ON seller.DepartmentId = department.Id " + "ORDER BY Name ");
+
+			rs = pst.executeQuery();
+
 			List<Seller> list = new ArrayList<>();
 			Map<Integer, Department> map = new HashMap<>();
-			
+
 			while (rs.next()) {
-				
+
 				Department dep = map.get(rs.getInt("DepartmentId"));
-				
+
 				if (dep == null) {
 					dep = instantiateDepartment(rs);
 					map.put(rs.getInt("DepartmentId"), dep);
 				}
-				
-				Seller seller = instantiateSeller(rs, dep);				
-				list.add(seller);			
+
+				Seller seller = instantiateSeller(rs, dep);
+				list.add(seller);
 			}
-			
-			return list;		
-			
+
+			return list;
+
 		} catch (SQLException e) {
-			
+
 			throw new DbException(e.getMessage());
 		} finally {
-			
+
 			DB.closeStatement(pst);
 			DB.closeResultSet(rs);
 		}
@@ -144,44 +173,42 @@ public class SellerDAOjdbc implements SellerDAO {
 
 	@Override
 	public List<Seller> findByDepartment(Department department) {
-		
+
 		PreparedStatement pst = null;
 		ResultSet rs = null;
-		
+
 		try {
 			pst = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "WHERE DepartmentId = ? "
-					+ "ORDER BY Name ");
-			
-			pst.setInt(1, department.getId());			
-			rs = pst.executeQuery(); // por padrão, essa consulta retorna como 0. Sendo assim, criamos uma condição if com rs.next() para pegar os dados desejados
-			
+					"SELECT seller.*,department.Name as DepName " 
+						+ "FROM seller INNER JOIN department "
+						+ "ON seller.DepartmentId = department.Id " + "WHERE DepartmentId = ? " + "ORDER BY Name ");
+
+			pst.setInt(1, department.getId());
+			rs = pst.executeQuery();
+
 			List<Seller> list = new ArrayList<>();
 			Map<Integer, Department> map = new HashMap<>();
-			
+
 			while (rs.next()) {
-				
+
 				Department dep = map.get(rs.getInt("DepartmentId"));
-				
+
 				if (dep == null) {
 					dep = instantiateDepartment(rs);
 					map.put(rs.getInt("DepartmentId"), dep);
 				}
-				
-				Seller seller = instantiateSeller(rs, dep);				
-				list.add(seller);			
+
+				Seller seller = instantiateSeller(rs, dep);
+				list.add(seller);
 			}
-			
-			return list;		
-			
+
+			return list;
+
 		} catch (SQLException e) {
-			
+
 			throw new DbException(e.getMessage());
 		} finally {
-			
+
 			DB.closeStatement(pst);
 			DB.closeResultSet(rs);
 		}
